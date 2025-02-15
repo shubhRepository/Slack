7dfc2bd7aa98620d31dbd93dbf9ddf05ee1e7336
Shubham Maheshwari <academicShubh@gmail.com>
2025-02-15 16:18:54 +0530

    create signin card

--- src/feature/auth/components/auth-screen.tsx
+++ src/feature/auth/components/auth-screen.tsx
@@ -3,13 +3,18 @@
 import { useState } from "react";
 import { SignInFlow } from "../types";
 import { SignInCard } from "./sign-in-card";
+import { SignUpCard } from "./sign-up-card";
 
 export const AuthScreen = () => {
   const [state, setState] = useState<SignInFlow>("signIn");
   return (
     <div className="h-full flex items-center justify-center bg-[#5c3B58]">
       <div className="md:h-auto md:w-[420px]">
-        {state === "signIn" && <SignInCard />}
+        {state === "signIn" ? (
+          <SignInCard setState={setState} />
+        ) : (
+          <SignUpCard />
+        )}
       </div>
     </div>
   );
