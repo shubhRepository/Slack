9b16c91891fb1e8cd163d64b315d687dbf05aeb5
Shubham Maheshwari <academicShubh@gmail.com>
2025-02-26 01:16:49 +0530

    create user workspace and get workspace ids

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,4 +1,28 @@
-import { query } from "./_generated/server";
+import { v } from "convex/values";
+import { query, mutation } from "./_generated/server";
+import { getAuthUserId } from "@convex-dev/auth/server";
+
+export const create = mutation({
+  args: {
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    // TODO: create a proper method later
+    const joinCode = "123456";
+
+    const workspaceId = await ctx.db.insert("workspaces", {
+      name: args.name,
+      userId,
+      joinCode,
+    });
+    return workspaceId;
+  },
+});
 
 export const get = query({
   args: {},
--- src/feature/workspaces/components/create-workspace-modal.tsx
+++ src/feature/workspaces/components/create-workspace-modal.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 
 import {
   Dialog,
@@ -7,13 +8,32 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+import { useCreateWorkSpace } from "../api/use-create-workspace";
 import { useCreateWorkSpaceModal } from "../store/use-create-workspace-modal";
 
 export const CreateWorkspaceModal = () => {
   const [open, setOpen] = useCreateWorkSpaceModal();
+  const [name, setName] = useState("");
+
+  const { mutate, isPending } = useCreateWorkSpace();
 
   const handleClose = () => {
     setOpen(false);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutate(
+      { name },
+      {
+        onSuccess(data) {
+          console.log(data);
+        },
+      }
+    );
   };
 
   return (
@@ -22,6 +42,20 @@ export const CreateWorkspaceModal = () => {
         <DialogHeader>
           <DialogTitle>Add a workspace</DialogTitle>
         </DialogHeader>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <Input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            disabled={isPending}
+            required
+            autoFocus
+            minLength={3}
+            placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+          />
+          <div className="flex justify-end">
+            <Button disabled={isPending}>Create</Button>
+          </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
