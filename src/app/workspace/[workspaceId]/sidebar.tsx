d620e37d16497f690186283668e5db3e4b9cd07a
Shubham Maheshwari <academicShubh@gmail.com>
2025-02-26 15:01:02 +0530

    add sidebar buttons

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,10 +1,16 @@
+import { Bell, Home, MessageSquare, MoreHorizontal } from "lucide-react";
 import { UserButton } from "@/feature/auth/components/user-button";
 import { WorkspaceSwitcher } from "./workspace-switcher";
+import { SidebarButton } from "./sidebar-button";
 
 export const Sidebar = () => {
   return (
     <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-4">
       <WorkspaceSwitcher />
+      <SidebarButton icon={Home} label="Home" isActive />
+      <SidebarButton icon={MessageSquare} label="DMs" />
+      <SidebarButton icon={Bell} label="Activity" />
+      <SidebarButton icon={MoreHorizontal} label="More" />
       <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
         <UserButton />
       </div>
