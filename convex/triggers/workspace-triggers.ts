e8c4587db53916e1fa0da645b6e12e71ba67da86
Shubham Maheshwari <academicShubh@gmail.com>
2025-02-26 22:49:57 +0530

    add workspace trigger to remove member when workspace is removed

---
+++ convex/triggers/workspace-triggers.ts
@@ -0,0 +1,29 @@
+import {
+  mutation as rawMutation,
+  internalMutation as rawInternalMutation,
+} from "../_generated/server";
+import { DataModel } from "../_generated/dataModel";
+import { Triggers } from "convex-helpers/server/triggers";
+import {
+  customCtx,
+  customMutation,
+} from "convex-helpers/server/customFunctions";
+
+const triggers = new Triggers<DataModel>();
+
+triggers.register("workspaces", async (ctx, change) => {
+  console.log("Inside workspace trigger");
+  if (change.operation === "delete") {
+    for await (const member of ctx.db
+      .query("members")
+      .withIndex("by_workspace_id", (q) => q.eq("workspaceId", change.id))) {
+      await ctx.db.delete(member._id);
+    }
+  }
+});
+
+export const mutation = customMutation(rawMutation, customCtx(triggers.wrapDB));
+export const internalMutation = customMutation(
+  rawInternalMutation,
+  customCtx(triggers.wrapDB)
+);
--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,5 +1,6 @@
 import { v } from "convex/values";
-import { query, mutation } from "./_generated/server";
+import { query } from "./_generated/server";
+import { mutation } from "./triggers/workspace-triggers";
 import { getAuthUserId } from "@convex-dev/auth/server";
 
 const generateCode = () => {
@@ -107,6 +108,34 @@ export const update = mutation({
     await ctx.db.patch(args.id, {
       name: args.name,
     });
+
+    return args.id;
+  },
+});
+
+export const remove = mutation({
+  args: {
+    id: v.id("workspaces"),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member || member.role !== "admin") {
+      throw new Error("Unauthorized");
+    }
+
+    await ctx.db.delete(args.id);
 
     return args.id;
   },
