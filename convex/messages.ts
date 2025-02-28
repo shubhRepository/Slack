1644f4e4d0ab4d559e9176ee736da4be16b53d13
Shubham Maheshwari <academicShubh@gmail.com>
2025-02-28 18:31:09 +0530

    add conversation id when the messages is inserted

--- convex/messages.ts
+++ convex/messages.ts
@@ -22,6 +22,7 @@ export const create = mutation({
     image: v.optional(v.id("_storage")),
     workspaceId: v.id("workspaces"),
     channelId: v.optional(v.id("channels")),
+    conversationId: v.optional(v.id("conversations")),
     parentMessageId: v.optional(v.id("messages")),
   },
   handler: async (ctx, args) => {
@@ -35,11 +36,24 @@ export const create = mutation({
     if (!member) {
       throw new Error("Unauthorized");
     }
+
+    let _conversationId = args.conversationId;
+
+    // Only possible if we are replying in a thread in 1:1 conversation
+    if (!args.conversationId && !args.channelId && args.parentMessageId) {
+      const parentMessage = await ctx.db.get(args.parentMessageId);
+      if (!parentMessage) {
+        throw new Error("Parent message not found");
+      }
+      _conversationId = parentMessage.conversationId;
+    }
+
     const messageId = await ctx.db.insert("messages", {
       body: args.body,
       memberId: member._id,
       image: args.image,
       channelId: args.channelId,
+      conversationId: _conversationId,
       workspaceId: args.workspaceId,
       parentMessageId: args.parentMessageId,
       updatedAt: Date.now(),
