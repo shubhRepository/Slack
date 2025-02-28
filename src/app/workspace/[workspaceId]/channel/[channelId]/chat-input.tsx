cfc8db4d21483f708ddb19e22e3d64f4652df75f
Shubham Maheshwari <academicShubh@gmail.com>
2025-02-28 10:41:39 +0530

    create editor component for typing messages

---
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -0,0 +1,9 @@
+import Editor from "@/components/editor";
+
+export const ChatInput = () => {
+  return (
+    <div className="px-5 w-full">
+      <Editor />
+    </div>
+  );
+};
--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -5,6 +5,7 @@ import { useChannelId } from "@/hooks/use-channel-id";
 
 import { Loader, TriangleAlert } from "lucide-react";
 import { Header } from "./header";
+import { ChatInput } from "./chat-input";
 
 const ChannelIdPage = () => {
   const channelId = useChannelId();
@@ -32,6 +33,8 @@ const ChannelIdPage = () => {
   return (
     <div className="flex flex-col h-full">
       <Header title={channel.name} />
+      <div className="flex flex-1" />
+      <ChatInput />
     </div>
   );
 };
